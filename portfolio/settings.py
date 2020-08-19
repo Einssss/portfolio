aa6a866c6aea3e430f0cf52a158174ae85e938d1
einsss <287911972b@gmail.com>
2020-08-19 01:09:40 -0400

    add local support

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -132,3 +132,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
+
